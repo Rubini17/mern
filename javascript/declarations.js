95c735c7f14a746206daadfbf96569b529fd2154
Rubini J S <rubinijs2004@gmail.com>
2025-02-14 12:09:51 +0530

    day6_fn

---
+++ javascript/declarations.js
@@ -0,0 +1,79 @@
+//PRIMARY DATA TYPES
+/*
+let a=10;
+var b="abcdef";
+const c=true;
+d= 12.300044948858858
+console.log(a,b,c, d);
+console.log(typeof(a),typeof(b), typeof(c),typeof(d));
+b=19;
+a="efg"
+//c=false;
+console.log(a,b,c, d);
+console.log(typeof(a),typeof(b), typeof(c),typeof(d));
+*/
+
+//SECONDARY DATA TYPES
+/*
+array=[1,2.44,"hell",[1,2,3]]
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+console.log(array[3])
+console.log(array)
+*/
+
+//OBJECTS
+obj1={
+    "name":"jakkk",
+    "age":18,
+    "class":"cse-c",
+    "sub":["eng","math","sci"]
+
+}
+//console.log(obj1);
+//or
+
+obj2={
+    
+};
+obj2["name"]="hell";
+obj2["age"]=1000;
+obj2["class"]="x";
+
+//console.log(obj2);
+
+//or
+
+obj3= new Object();
+obj3["name"]="hellonn";
+obj3["age"]=1000;
+obj3["class"]="x";
+
+//console.log(obj3);
+//console.log(obj3.class);
+//console.log(obj3['age']);
+
+//or
+
+obj4= new Object();
+obj4.name="loooll";
+//console.log(obj4);
+
+//Set
+
+set1= new Set();
+//console.log(set1);
+
+set2= new Set("hello");
+//console.log(set2);
+
+set3= new Set();
+set3.add("hellooo");
+//console.log(set3);
+
+set4= new Set(["efffgt"]);
+console.log(set4);
+set4.add("fjffh");
+set4.add("efffgt");
+console.log(set4);
